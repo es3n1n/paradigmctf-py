d8c0dfc293fdb24f470b69f35c4afd5b468e3dfb
es3n1n <git@es3n1n.im>
2025-07-17 10:21:52 +0200

    refactor: remove redundant imports in examples

--- examples/hello/challenge.py
+++ examples/hello/challenge.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from typing import List
-
-from web3 import Web3
-
 from ctf_launchers.pwn_launcher import PwnChallengeLauncher
-from ctf_launchers.types import ChallengeContract
 
 
 class Launcher(PwnChallengeLauncher):
